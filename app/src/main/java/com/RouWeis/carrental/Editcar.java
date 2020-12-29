4555fc95ece9c51d0dd5f45a4aec696e06ae1b37
Rouwaida Ayed <58150060+Rouwaida-ayed@users.noreply.github.com>
2020-12-30 00:47:13 +0100

    Merge branch 'main' of https://github.com/YassineAsmi/CarRental into main
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/layout/sign_up.xml

--- app/src/main/java/com/RouWeis/carrental/Editcar.java
+++ app/src/main/java/com/RouWeis/carrental/Editcar.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class compte extends AppCompatActivity {
+public class Editcar extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_compte);
-
+        setContentView(R.layout.activity_editcar);
     }
 }
