1810ce3a16ec6cb3e6d67f8530e394d714ba99f7
YassineAsmi <iyazasmi@gmail.com>
2021-01-12 01:43:43 +0100

    compte

--- app/src/main/java/com/RouWeis/carrental/compte.java
+++ app/src/main/java/com/RouWeis/carrental/compte.java
@@ -1,15 +1,62 @@
 package com.RouWeis.carrental;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class compte extends AppCompatActivity {
+
+    private TextView tel;
+    private TextView name;
+    private TextView mail;
+    private FirebaseAuth auth;
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compte);
+        name = findViewById(R.id.name1);
+        mail = findViewById(R.id.email1);
+        tel = findViewById(R.id.phone1);
+        auth = FirebaseAuth.getInstance();
+        userID=auth.getCurrentUser().getUid();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        DocumentReference documentReference = db.collection("users").document(userID);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                    name.setText(value.getString("Name"));
+                    Log.d("readcompte","name:"+name);
+                    mail.setText(value.getString("Email"));
+                Log.d("readcompte","mail:"+mail);
+                tel.setText(value.getString("Tel"));
+                Log.d("readcompte","tel:"+tel);
+
+
+            }
+        });
+
 
     }
 }
